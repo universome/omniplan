aa1a5e3d843cfb88991acf514caedb27dca12b82
revers <revers@yandex-team.ru>
2015-09-13 16:26:41 +0300

    Data line

--- client/components/CalendarHeader/CalendarHeader.js
+++ client/components/CalendarHeader/CalendarHeader.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import PlanStore from 'stores/PlanStore';
 
 class CalendarHeader extends React.Component {
+    constructor(...args) {
+        super(args);
+        this.state = {
+            plan: PlanStore.getPlan()
+        };
+    }
+
+    componentDidMount() {
+        PlanStore.on('change', plan => this.setState({plan}));
+    }
+
     render() {
+        var startDate = '';
+        var dates = [];
+
+        if (this.state.plan.creationDate) {
+            var startDate = Number(this.state.plan.creationDate.slice(8, 10));
+            for (let i = 0; i < 20; i++) {
+                dates.push(<span key={startDate + i} style={{display: 'inline-block', width: 23, border: 'solid grey', borderWidth: '0 1px', textAlign: 'center'}}>{startDate + i}</span>);
+            }
+        }
+
         return (
             <div>
                 <div>
-                    <h2>CalendarHeader</h2>
+                    <h2>CalendarHeader {startDate}</h2>
+                </div>
+                <div>
+                    {dates}
                 </div>
             </div>
         );
