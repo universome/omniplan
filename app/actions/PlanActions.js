dd8c55aad8caaf79a9f59f00c02a36da6d17b040
Ivan Skorokhodov <iskorokhodov@gmail.com>
2015-09-14 13:11:36 +0300

    Changed reqwest to jquery for now (we will remove it later)

--- app/actions/PlanActions.js
+++ app/actions/PlanActions.js
@@ -1,5 +1,6 @@
 import AppDispatcher from 'AppDispatcher';
-import reqwest from 'reqwest';
+// import reqwest from 'reqwest';
+import $ from 'jquery';
 
 var _planLastFetchTime = 0;
 
@@ -10,15 +11,13 @@ class PlanActionsClass {
             return; // Do not fetch projects every time we visit the page
         }
 
-		reqwest({
-            url: config.API_URL + '/getPlan',
-            crossOrigin: true,
-            success: plan => {
-    			AppDispatcher.dispatch({actionType: 'plan:fetch', plan: plan});
-    			_planLastFetchTime = Date.now();
-    		},
-            error: console.warn.bind(console)
-        });
+        $
+            .getJSON(config.API_URL + '/getPlan')
+            .fail(console.warn.bind(console))
+            .success(plan => {
+                AppDispatcher.dispatch({actionType: 'plan:fetch', plan: plan});
+                _planLastFetchTime = Date.now();
+            });
     }
 }
 
